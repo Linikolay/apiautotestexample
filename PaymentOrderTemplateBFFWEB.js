bfa38b9e9591f18168040e6ddebed50e0c1af9f0
Hhxh Sysh01~ <linikolay94@gmail.com>
2023-07-05 17:57:18 +0500

    tttt

--- PaymentOrderTemplateBFFWEB.js
+++ PaymentOrderTemplateBFFWEB.js
@@ -126,7 +126,7 @@ module.exports = function generate() {
 
     describe('TemplateBFF Check Create Get Template', async () => {
         it('/api/business/' + business + '/' + branch +'/paymentOrdersTemplate/' + templateID, async () => {
-            const res = await axios.get(webURl + "/api/business/" + business + '/' + branch +"/paymentOrdersTemplate/" + templateID, {
+            const res = await axios.get(webURl + "/api/business/" + business + '/' + branch +"/paymentOrdersTemplate333/" + templateID, {
                 headers: {
                     "X-Device-Info": deviceinfo,
                     Authorization: 'Bearer' + ' ' + token1
